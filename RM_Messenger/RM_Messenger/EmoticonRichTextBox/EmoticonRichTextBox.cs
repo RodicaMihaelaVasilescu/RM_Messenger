30b29a3b49625fe8d5294180e010ca3a1d76ffb7
Rodica Vasilescu <vasilescu.rodica@yahoo.com>
2020-02-15 19:42:52 +0200

    move emoticons from xaml to c#, remove comments

--- RM_Messenger/RM_Messenger/EmoticonRichTextBox/EmoticonRichTextBox.cs
+++ RM_Messenger/RM_Messenger/EmoticonRichTextBox/EmoticonRichTextBox.cs
@@ -1,10 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Threading;
@@ -14,27 +12,45 @@ namespace RM_Messenger
 {
   public class EmoticonRichTextBox : RichTextBox
   {
-    private DispatcherTimer _timer;
-    // Document property
-    //public static readonly DependencyProperty DocumentProperty =
-    //    DependencyProperty.Register("Document", typeof(FlowDocument),
-    //    typeof(EmoticonRichTextBox), new PropertyMetadata(OnDocumentChanged));
-    //private static void OnDocumentChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
-    //{
-
-    //}
-
-    //public string Document
-    //{
-    //  get { return (string)GetValue(DocumentProperty); }
-    //  set { SetValue(DocumentProperty, value); }
-    //}
-
-
     public EmoticonRichTextBox()
     {
       Emoticons = new EmoticonCollection();
+      Emoticons.Add(new EmoticonMapper { Icon = "pack://application:,,,/RM_Messenger;component/Resources/Emoticons/1.gif", Text = ":)" });
+      Emoticons.Add(new EmoticonMapper { Icon = "pack://application:,,,/RM_Messenger;component/Resources/Emoticons/2.gif", Text = ":(" });
+      Emoticons.Add(new EmoticonMapper { Icon = "pack://application:,,,/RM_Messenger;component/Resources/Emoticons/3.gif", Text = ";)" });
+      Emoticons.Add(new EmoticonMapper { Icon = "pack://application:,,,/RM_Messenger;component/Resources/Emoticons/4.gif", Text = ":D" });
+      Emoticons.Add(new EmoticonMapper { Icon = "pack://application:,,,/RM_Messenger;component/Resources/Emoticons/5.gif", Text = ";;)" });
+      Emoticons.Add(new EmoticonMapper { Icon = "pack://application:,,,/RM_Messenger;component/Resources/Emoticons/6.gif", Text = ">:D<" });
+      Emoticons.Add(new EmoticonMapper { Icon = "pack://application:,,,/RM_Messenger;component/Resources/Emoticons/7.gif", Text = ":-/" });
+      Emoticons.Add(new EmoticonMapper { Icon = "pack://application:,,,/RM_Messenger;component/Resources/Emoticons/7.gif", Text = ":/" });
+      Emoticons.Add(new EmoticonMapper { Icon = "pack://application:,,,/RM_Messenger;component/Resources/Emoticons/8.gif", Text = ":x" });
+      Emoticons.Add(new EmoticonMapper { Icon = "pack://application:,,,/RM_Messenger;component/Resources/Emoticons/8.gif", Text = ":X" });
+      Emoticons.Add(new EmoticonMapper { Icon = "pack://application:,,,/RM_Messenger;component/Resources/Emoticons/9.gif", Text = ":>" });
+      Emoticons.Add(new EmoticonMapper { Icon = "pack://application:,,,/RM_Messenger;component/Resources/Emoticons/10.gif", Text = ":p" });
+      Emoticons.Add(new EmoticonMapper { Icon = "pack://application:,,,/RM_Messenger;component/Resources/Emoticons/10.gif", Text = ":-p" });
+      Emoticons.Add(new EmoticonMapper { Icon = "pack://application:,,,/RM_Messenger;component/Resources/Emoticons/10.gif", Text = ":P" });
+      Emoticons.Add(new EmoticonMapper { Icon = "pack://application:,,,/RM_Messenger;component/Resources/Emoticons/10.gif", Text = ":-P" });
+      Emoticons.Add(new EmoticonMapper { Icon = "pack://application:,,,/RM_Messenger;component/Resources/Emoticons/11.gif", Text = ":*" });
+      Emoticons.Add(new EmoticonMapper { Icon = "pack://application:,,,/RM_Messenger;component/Resources/Emoticons/12.gif", Text = "=((" });
+      Emoticons.Add(new EmoticonMapper { Icon = "pack://application:,,,/RM_Messenger;component/Resources/Emoticons/13.gif", Text = ":o" });
+      Emoticons.Add(new EmoticonMapper { Icon = "pack://application:,,,/RM_Messenger;component/Resources/Emoticons/14.gif", Text = "x(" });
+      Emoticons.Add(new EmoticonMapper { Icon = "pack://application:,,,/RM_Messenger;component/Resources/Emoticons/15.gif", Text = ":>" });
+      Emoticons.Add(new EmoticonMapper { Icon = "pack://application:,,,/RM_Messenger;component/Resources/Emoticons/16.gif", Text = "b-)" });
+      Emoticons.Add(new EmoticonMapper { Icon = "pack://application:,,,/RM_Messenger;component/Resources/Emoticons/17.gif", Text = ":s" });
+      Emoticons.Add(new EmoticonMapper { Icon = "pack://application:,,,/RM_Messenger;component/Resources/Emoticons/18.gif", Text = "#:-S" });
+      Emoticons.Add(new EmoticonMapper { Icon = "pack://application:,,,/RM_Messenger;component/Resources/Emoticons/19.gif", Text = ">:)" });
+      Emoticons.Add(new EmoticonMapper { Icon = "pack://application:,,,/RM_Messenger;component/Resources/Emoticons/20.gif", Text = ":((" });
+      Emoticons.Add(new EmoticonMapper { Icon = "pack://application:,,,/RM_Messenger;component/Resources/Emoticons/21.gif", Text = ":))" });
+      Emoticons.Add(new EmoticonMapper { Icon = "pack://application:,,,/RM_Messenger;component/Resources/Emoticons/22.gif", Text = ":|" });
+      Emoticons.Add(new EmoticonMapper { Icon = "pack://application:,,,/RM_Messenger;component/Resources/Emoticons/23.gif", Text = "/:)" });
+      Emoticons.Add(new EmoticonMapper { Icon = "pack://application:,,,/RM_Messenger;component/Resources/Emoticons/24.gif", Text = "=))" });
+      Emoticons.Add(new EmoticonMapper { Icon = "pack://application:,,,/RM_Messenger;component/Resources/Emoticons/36.gif", Text = "<:-P" });
+      Emoticons.Add(new EmoticonMapper { Icon = "pack://application:,,,/RM_Messenger;component/Resources/Emoticons/36.gif", Text = "<:-p" });
+      Emoticons.Add(new EmoticonMapper { Icon = "pack://application:,,,/RM_Messenger;component/Resources/Emoticons/65.gif", Text = ":-\"" });
+      Emoticons.Add(new EmoticonMapper { Icon = "pack://application:,,,/RM_Messenger;component/Resources/Emoticons/102.gif", Text = "~x(" });
     }
+
+    private DispatcherTimer _timer;
 
     /// <summary>
     /// Override to trigger the look up.
