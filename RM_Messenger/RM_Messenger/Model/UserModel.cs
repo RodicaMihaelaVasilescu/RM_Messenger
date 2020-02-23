bf2e72ab405c330c9100fd46540fea5e8ae12262
Rodica Vasilescu <vasilescu.rodica@yahoo.com>
2020-02-23 22:45:54 +0200

    remove empty lines

--- RM_Messenger/RM_Messenger/Model/UserModel.cs
+++ RM_Messenger/RM_Messenger/Model/UserModel.cs
@@ -3,18 +3,13 @@
   class UserModel
   {
     private static UserModel _instance;
-
     public static UserModel Instance => _instance ?? (_instance = new UserModel());
-
     public string Status { get; set; }
     public string Username { get; set; }
     public string ID { get; set; }
     public byte[] ProfilePicture { get; set; }
-
     public string FirstName { get; set; }
-
     public string LastName { get; set; }
-
     public bool IsOnline { get; set; } = true;
     public string EncryptedPassword { get; set; }
 
