02b6969578f5feba329c0838901e92a38293fa7d
Rodica Vasilescu <vasilescu.rodica@yahoo.com>
2020-02-23 22:45:06 +0200

    remove unused namespace

--- RM_Messenger/RM_Messenger/Model/MessageModel.cs
+++ RM_Messenger/RM_Messenger/Model/MessageModel.cs
@@ -1,12 +1,10 @@
-﻿using System.Windows;
-using System.Windows.Documents;
+﻿using System.Windows.Documents;
 
 namespace RM_Messenger.Model
 {
   class MessageModel
   {
     public string SentBy { get; set; }
-
     public string ToolTip { get; set; }
     public string SentTo { get; set; }
     public FlowDocument Content { get; set; }
