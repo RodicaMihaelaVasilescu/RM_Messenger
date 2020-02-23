f3f7b41bd1b4fee26c836e3fe05bd5833281310f
Rodica Vasilescu <vasilescu.rodica@yahoo.com>
2020-02-23 23:11:52 +0200

    remove comments and namespaces

--- RM_Messenger/RM_Messenger/View/CreateNewAccountControl.xaml.cs
+++ RM_Messenger/RM_Messenger/View/CreateNewAccountControl.xaml.cs
@@ -1,20 +1,8 @@
-﻿using RM_Messenger.Helper;
-using RM_Messenger.Helpers;
+﻿using RM_Messenger.Helpers;
 using RM_Messenger.Model;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace RM_Messenger.View
 {
